eaab0257ec9f5c3c54e9bf48b721bc2cb0b95ac8
vagrant <vagrant@precise64.(none)>
2015-05-28 10:07:19 +0000

    Fetch faac.

--- init_update_libs.sh
+++ init_update_libs.sh
@@ -7,9 +7,11 @@ echo "============================================"
 echo "Updating libpng, expat and fribidi"
 rm -rf libpng-*
 rm -rf expat-*
+rm -rf faac-*
 rm -rf fribidi-*
 
 wget -O- http://downloads.sourceforge.net/project/libpng/libpng16/older-releases/1.6.13/libpng-1.6.13.tar.xz | tar xJ
 wget -O- http://downloads.sourceforge.net/project/expat/expat/2.1.0/expat-2.1.0.tar.gz | tar xz
+wget -O- http://downloads.sourceforge.net/project/faac/faac-src/faac-1.28/faac-1.28.tar.gz | tar xz
 wget -O- http://fribidi.org/download/fribidi-0.19.6.tar.bz2 | tar xj
 echo "============================================"
