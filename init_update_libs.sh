651c682a6e640ed9b726845261752cd48385e4eb
vagrant <vagrant@precise64.(none)>
2015-05-28 10:59:32 +0000

    Put new config

--- init_update_libs.sh
+++ init_update_libs.sh
@@ -15,3 +15,9 @@ wget -O- http://downloads.sourceforge.net/project/expat/expat/2.1.0/expat-2.1.0.
 wget -O- http://downloads.sourceforge.net/project/faac/faac-src/faac-1.28/faac-1.28.tar.gz | tar xz
 wget -O- http://fribidi.org/download/fribidi-0.19.6.tar.bz2 | tar xj
 echo "============================================"
+
+echo "Putting new config"
+cp assets/config.sub faac-1.28/config.sub
+cp assets/config.guess faac-1.28/config.guess
+echo "============================================"
+
